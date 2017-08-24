cf6bb4d0cad67de1c5415c9602f2353bf437a442
Dan Wright <wright@pair.com>
2017-08-24 19:46:19 -0400

    More commands

--- bin/drip_client.pl
+++ bin/drip_client.pl
@@ -56,14 +56,44 @@ Accepts: -email, -id, -new_email, -user_id, -time_zone, -lifetime_value, -ip_add
 
 Delete a subscriber.   Must specify -email, or -id.
 
+=item B<getwork>
+
+Get a list of all workflows.
+
+=item B<startwork>
+
+Start a user on a workflow
+
+Requires: -workflow
+
+Accepts: -email, -id, -user_id, -time_zone, -prospect
+
+=item B<event>
+
+Record that an event happened.
+
+Requires: -email or -id, -action
+
+Accepts: -prospect
+
+
 =back
 
 =head2 Additional Parameters
 
 The parameters B<-email>, B<-id>, B<-new_email>, B<-user_id>, B<-time_zone>,
-B<-lifetime_vaule>, and B<-ip_address> are documented at
+B<-lifetime_vaule>, B<-ip_address>, B<-prospect>, B<-action> are documented at
 L<https://www.getdrip.com/docs/rest-api> and may be used with the above
 operations.
+
+=over
+
+=item B<workflow>
+
+Integer ID of a workflow.
+
+
+=back
 
 =head1 DESCRIPTION
 
@@ -91,8 +121,9 @@ GetOptions( \%OPT,
     'man',
     'verbose|verb|v+',
     'conf=s',
-    'addsub', 'getsub', 'delsub',
-    'email=s', 'id=s', 'new_email=s', 'user_id=s', 'time_zone=s', 'lifetime_vaule=i', 'ip_address=s', 
+    'addsub', 'getsub', 'delsub', 'getwork', 'startwork', 'event',
+    'workflow=i', 'action=s',
+    'email=s', 'id=s', 'new_email=s', 'user_id=s', 'time_zone=s', 'lifetime_vaule=i', 'ip_address=s', 'prospect'
 ) or pod2usage(2);
 pod2usage(1) if $OPT{help};
 pod2usage(-exitval => 0, -verbose => 2) if $OPT{man};
@@ -105,6 +136,9 @@ eval {
     if ( $OPT{getsub} ) {  get_subscribers() and exit }
     if ( $OPT{addsub} ) {  add_subscribers( %OPT ) and exit }
     if ( $OPT{delsub} ) {  delete_subscribers( %OPT ) and exit }
+    if ( $OPT{getwork} ) { get_workflows( %OPT ) and exit }
+    if ( $OPT{startwork} ) { start_workflow( %OPT ) and exit }
+    if ( $OPT{event} ) { record_event( %OPT ) and exit }
 };
 if ( $@ ) {
     warn "Request failed:";
@@ -112,6 +146,38 @@ if ( $@ ) {
 }
 
 exit;
+
+sub record_event {
+    my %OPT = @_;
+
+    $OPT{email} or $OPT{id} or pod2usage( "Either -email or -id is required for -event" );
+    $OPT{action} or pod2usage( "-action is required for -event" );
+
+    my $content = _build_hash( %OPT, keys => [qw(email id action prospect)] );
+    my $result = $client->do_request( POST => 'events', { events => [ $content ] } );
+    p $result;
+}
+
+
+sub get_workflows {
+    my %OPT = @_;
+
+    my $result = $client->do_request( GET => 'workflows' );
+    p $result;
+}
+
+
+sub start_workflow {
+    my %OPT = @_;
+    $OPT{workflow} or pod2usage( "Required parameter -workflow missing for -startwork" );
+
+    my $subscriber = _build_hash( %OPT, keys => [qw( email id user_id time_zone prospect )] );
+
+    my $endpoint = 'workflows/' . $OPT{workflow} . '/subscribers';
+
+    my $result = $client->do_request( POST => $endpoint, { subscribers => [ $subscriber ] } );
+    p $result;
+}
 
 
 sub delete_subscribers {
@@ -127,21 +193,21 @@ sub delete_subscribers {
 
 sub add_subscribers {
     my %OPT = @_;
-    my $subscriber = _build_subscriber_hash( %OPT );
+    my $subscriber = _build_hash( %OPT, keys => [qw( email id new_email user_id time_zone lifetime_value ip_address )] );
     die "email or id required in add subscriber" unless $subscriber->{email} or $subscriber->{id};
 
     my $result = $client->do_request( POST => 'subscribers', { subscribers => [ $subscriber ]});
     p $result;
 }
 
-sub _build_subscriber_hash {
+sub _build_hash {
     my %OPT = @_;
-    my $subscriber = {};
-    foreach my $key ( qw( email id new_email user_id time_zone lifetime_value ip_address ) ) {
+    my $build = {};
+    foreach my $key ( @{$OPT{keys}} ) {
         next unless defined $OPT{$key};
-        $subscriber->{$key} = $OPT{$key};
+        $build->{$key} = $OPT{$key};
     }
-    return $subscriber;
+    return $build;
 }
 
 sub get_subscribers {
