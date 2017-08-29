f77717149d480229b5acba86fbf1800104b5f56b
Dan Wright <wright@pair.com>
2017-08-29 19:29:15 -0400

    0.04 -- Fixed tests

--- lib/API/Drip/Request.pm
+++ lib/API/Drip/Request.pm
@@ -31,11 +31,11 @@ API::Drip::Request - Perl interface to api.getdrip.com
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
